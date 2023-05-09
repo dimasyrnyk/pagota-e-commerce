2cafd970d7aa8ff1340bf4818c4e80c6f60bdb42
dimasyrnyk <syrnykdmitriy@gmail.com>
2023-05-09 17:41:22 +0300

    added links to navigate between pages

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 import "./NotFoundPage.scss";
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="not-found__container">
+      <h2>NotFoundPage</h2>
+      <Link to="/">HomePage</Link>
+      <Link to="/cart">CartPage</Link>
+      <Link to="/products">ProductsListPage</Link>
+      <Link to="/products/id">ProductItemPage</Link>
+      <Link to="*">NotFoundPage</Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
--- src/pages/ProductsListPage/index.tsx
+++ src/pages/ProductsListPage/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 import "./ProductsListPage.scss";
 
 function ProductsListPage() {
-  return <div>ProductsListPage</div>;
+  return (
+    <div className="products-list__container">
+      <h2>ProductsListPage</h2>
+      <Link to="/">HomePage</Link>
+      <Link to="/cart">CartPage</Link>
+      <Link to="/products">ProductsListPage</Link>
+      <Link to="/products/id">ProductItemPage</Link>
+      <Link to="*">NotFoundPage</Link>
+    </div>
+  );
 }
 
 export default ProductsListPage;
