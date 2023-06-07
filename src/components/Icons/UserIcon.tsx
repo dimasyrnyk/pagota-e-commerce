22d08b79cc7ca14a1b8ee2558a7d4cfe014d3e5f
dimasyrnyk <syrnykdmitriy@gmail.com>
2023-06-07 09:57:26 +0300

    added user picture instead of user icon when user is auth

--- src/components/Icons/UserIcon.tsx
+++ src/components/Icons/UserIcon.tsx
@@ -1,3 +1,7 @@
+import { useSelector } from "react-redux";
+
+import "./Icons.scss";
+import { RootState } from "@store/index";
 import userIcon from "@assets/icons/userIcon.svg";
 
 type Props = {
@@ -6,10 +10,11 @@ type Props = {
 };
 
 function UserIcon({ className = "", onClick }: Props) {
+  const { user } = useSelector((state: RootState) => state.auth);
   return (
     <img
-      className={className}
-      src={userIcon}
+      className={"user__icon " + className}
+      src={user ? user.picture : userIcon}
       alt="User icon"
       onClick={onClick}
     />
